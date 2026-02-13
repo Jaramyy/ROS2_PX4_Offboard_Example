94dd3cdb6f1d0bb62ac219b070f98b0980fde225
jaramyy <jamephet@gmail.com>
2026-02-13 19:04:33 +0900

    change repo

--- px4_offboard/setup.py
+++ px4_offboard/setup.py
@@ -31,7 +31,8 @@ setup(
                 'visualizer = px4_offboard.visualizer:main',
                 'velocity_control = px4_offboard.velocity_control:main',
                 'control = px4_offboard.control:main',
-                'processes = px4_offboard.processes:main'
+                'processes = px4_offboard.processes:main',
+                'PA_velocity_control = px4_offboard.PA_velocity_control:main',
         ],
     },
 )
